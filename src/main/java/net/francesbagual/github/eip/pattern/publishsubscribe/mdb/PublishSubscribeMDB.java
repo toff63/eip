effd2dd4cc181e8ebe41143a7785a2048831ffab
Christophe Marchal <toff63@gmail.com>
2014-07-18 13:21:24 -0300

    added the publish subscribe pattern

---
+++ src/main/java/net/francesbagual/github/eip/pattern/publishsubscribe/mdb/PublishSubscribeMDB.java
@@ -0,0 +1,34 @@
+package net.francesbagual.github.eip.pattern.publishsubscribe.mdb;
+
+import java.util.logging.Logger;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@MessageDriven(name = "PublishSubscribeMDB", activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "topic/publishsubscribe"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
+public class PublishSubscribeMDB implements MessageListener {
+
+	private final static Logger LOGGER = Logger.getLogger(PublishSubscribeMDB.class.toString());
+
+	@Override
+	public void onMessage(Message receivedMessage) {
+		TextMessage msg = null;
+		try {
+			if (receivedMessage instanceof TextMessage) {
+				msg = (TextMessage) receivedMessage;
+				LOGGER.info("Received Message from topic: " + msg.getText());
+			} else {
+				LOGGER.warning("Message of wrong type: " + receivedMessage.getClass().getName());
+			}
+		} catch (JMSException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ src/main/java/net/francesbagual/github/eip/pattern/publishsubscribe/servlet/PublishSubscribeServlet.java
@@ -0,0 +1,61 @@
+package net.francesbagual.github.eip.pattern.publishsubscribe.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.JMSDestinationDefinition;
+import javax.jms.JMSDestinationDefinitions;
+import javax.jms.Topic;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@JMSDestinationDefinitions(
+		value = {
+				@JMSDestinationDefinition(
+						name = "jms/topic/publishsubscribe",
+						interfaceName = "javax.jms.Topic",
+						destinationName = "publishsubscribe"
+				)
+		})
+@WebServlet("/publishsubscribe/client")
+public class PublishSubscribeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private JMSContext context;
+
+	@Resource(lookup = "jms/topic/publishsubscribe")
+	private Topic topic;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.write("<h1>Quickstart: Example demonstrates the use of <strong>JMS 2.0</strong> and <strong>EJB 3.2 Message-Driven Bean</strong> in WildFly 8.</h1>");
+		try {
+			final Destination destination = topic;
+
+			out.write("<p>Sending messages to <em>" + destination + "</em></p>");
+			out.write("<h2>Following messages will be send to the destination:</h2>");
+			context.createProducer().send(destination, "Hello world, publish subscribe");
+			out.write("<p><i>Go to your WildFly Server console or Server log to see the result of messages processing</i></p>");
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+}
