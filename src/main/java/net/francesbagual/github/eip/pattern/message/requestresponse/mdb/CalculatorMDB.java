3e64e28f3aa7511388a887d07fedf4bdef04c3ce
Christophe Marchal <toff63@gmail.com>
2014-07-27 22:16:36 -0300

    Added request response
    * implementation using the temporary queue
    * implementation using a fixed queue and the correlatio id

--- src/main/java/net/francesbagual/github/eip/pattern/message/requestresponse/mdb/CalculatorMDB.java
+++ src/main/java/net/francesbagual/github/eip/pattern/message/requestresponse/mdb/CalculatorMDB.java
@@ -6,10 +6,13 @@ import java.util.logging.Logger;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.inject.Inject;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
+import javax.jms.TemporaryQueue;
 
 import net.francesbagual.github.eip.pattern.message.requestresponse.message.CalculatorRequest;
 import net.francesbagual.github.eip.pattern.message.requestresponse.message.CalculatorRequestMessage;
@@ -25,26 +28,21 @@ public class CalculatorMDB implements MessageListener {
 
 	@Inject @RequestResponseProfile
 	private CalculatorAPI calculator;
-
+	
+	@Inject
+	private JMSContext context;
+	
 	@Override
 	public void onMessage(Message receivedMessage) {
 		try {
 			if (receivedMessage instanceof ObjectMessage) {
 				ObjectMessage msg = (ObjectMessage) receivedMessage;
+				Destination responseQueue = msg.getJMSReplyTo();
 				CalculatorRequestMessage calculatorRequestMessage = (CalculatorRequestMessage) msg.getObject();
 				String operation = (String) calculatorRequestMessage.headers().get("operation");
+				String correlationID = msg.getJMSMessageID();
 				CalculatorRequest request = (CalculatorRequest) calculatorRequestMessage.body();
-				switch (operation) {
-					case "add":
-						sendResponse(calculator.add(request));
-						break;
-					case "mult":
-						sendResponse(calculator.multiply(request));
-						break;
-					default:
-						LOGGER.warning("Unknown operation: " + operation);
-						break;
-				}
+				callService(responseQueue, operation, correlationID, request);
 			} else {
 				LOGGER.warning("Message of wrong type: " + receivedMessage.getClass().getName());
 			}
@@ -53,7 +51,30 @@ public class CalculatorMDB implements MessageListener {
 		}
 	}
 
-	private void sendResponse(BigDecimal result) {
+	private void callService(Destination responseQueue, String operation, String correlationID, CalculatorRequest request) throws JMSException {
+		switch (operation) {
+			case "add":
+				sendResponse(responseQueue, calculator.add(request), correlationID);
+				break;
+			case "mult":
+				sendResponse(responseQueue, calculator.multiply(request), correlationID);
+				break;
+			default:
+				LOGGER.warning("Unknown operation: " + operation);
+				break;
+		}
+	}
+
+	private void sendResponse(Destination responseQueue, BigDecimal result, String correlationId) throws JMSException {
 		LOGGER.info("Result: " + result);
+		if(responseQueue instanceof TemporaryQueue) context.createProducer().send(responseQueue, result);
+		else context.createProducer().send(responseQueue, jmsMessage(responseQueue, result, correlationId));
+	}
+
+	private Message jmsMessage(Destination responseQueue, BigDecimal result, String correlationId) throws JMSException {
+		Message msg = context.createObjectMessage(result);
+		msg.setJMSReplyTo(responseQueue);
+		msg.setJMSCorrelationID(correlationId);
+		return msg;
 	}
 }
--- src/main/java/net/francesbagual/github/eip/pattern/message/requestresponse/servlet/RequestResponseServlet.java
+++ src/main/java/net/francesbagual/github/eip/pattern/message/requestresponse/servlet/RequestResponseServlet.java
@@ -5,13 +5,17 @@ import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.annotation.Resource;
 import javax.inject.Inject;
 import javax.jms.JMSContext;
 import javax.jms.JMSDestinationDefinition;
 import javax.jms.JMSDestinationDefinitions;
+import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.Queue;
+import javax.jms.TemporaryQueue;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,6 +33,11 @@ import org.apache.commons.collections4.map.HashedMap;
 						name = "jms/queue/calculator",
 						interfaceName = "javax.jms.Queue",
 						destinationName = "calculator"
+				),
+				@JMSDestinationDefinition(
+						name = "jms/queue/calculatorresponse",
+						interfaceName = "javax.jms.Queue",
+						destinationName = "calculatorresponse"
 				)
 		})
 @WebServlet("/requestresponse")
@@ -38,26 +47,60 @@ public class RequestResponseServlet extends HttpServlet {
 	@Resource(lookup = "jms/queue/calculator")
 	private Queue calculatorRequestQueue;
 
+	@Resource(lookup = "jms/queue/calculatorresponse")
+	private Queue calculatorResponseQueue;
+
 	@Inject
 	private JMSContext context;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
 		out.write("Request response example");
 		out.write("<br />");
 		out.write("Sending an addition of 1 + 1 using temporary queue: the temporary queue on live until the connection is closed. There is no guaranteed delivery with this technique.");
-		context.createProducer().send(calculatorRequestQueue, create1Plus1Message(context.createTemporaryQueue()));
-		out.write("<p><i>Go to your WildFly Server console or Server log to see the result of messages processing</i></p>");
+		TemporaryQueue responseQueue = context.createTemporaryQueue();
+		context.createProducer().send(calculatorRequestQueue, messageWithReplyTo(responseQueue, create1Plus1Message()));
+		BigDecimal result = context.createConsumer(responseQueue).receiveBody(BigDecimal.class);
+		out.write("<p><i>Result received from service: " + result + "</i></p>");
+		out.write("Sending an addition of 1 + 1 using a dedicated queue with correlationID");
+		String messageId = UUID.randomUUID().toString();
+		context.createProducer().send(calculatorRequestQueue, messageWithReplyTo(calculatorResponseQueue, messageId, create1Plus1Message()));
+		Message response = context.createConsumer(calculatorResponseQueue).receive(1000L);
+		try {
+			if(response.getJMSCorrelationID().equals(messageId)) out.write("<p><i>Result received from service: " + response.getBody(BigDecimal.class) + "</i></p>");
+			else out.write("Received the response from another request (wrong correlation id)");
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
 
 	}
 
-	private CalculatorRequestMessage create1Plus1Message(Queue responseQueue){
+	private Message messageWithReplyTo(Queue responseQueue, String messageId, CalculatorRequestMessage request)  {
+		Message msg = messageWithReplyTo(responseQueue, request);
+		try {
+			msg.setJMSMessageID(messageId);
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+		return msg;
+	}
+
+	private Message messageWithReplyTo(Queue responseQueue, CalculatorRequestMessage request)  {
+		Message msg = context.createObjectMessage(request);
+		try {
+			msg.setJMSReplyTo(responseQueue);
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+		return msg;
+	}
+
+	private CalculatorRequestMessage create1Plus1Message() {
 		CalculatorRequest request = new CalculatorRequest(Arrays.asList(BigDecimal.valueOf(1), BigDecimal.valueOf(1)));
-		Map<String, Object> headers  = new HashedMap<String, Object>();
+		Map<String, Object> headers = new HashedMap<String, Object>();
 		headers.put("operation", "add");
-		headers.put("response", responseQueue);
 		return new CalculatorRequestMessage(headers, request);
 	}
 }
